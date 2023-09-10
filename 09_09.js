1386d4d33d1b676d5b95d7877093ef50e073727d
nhientattran <nhientattran@gmail.com>
2023-09-09 23:55:51 -0500

    09/09/2023

---
+++ 09_09.js
@@ -0,0 +1,71 @@
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+function rowSumOddNumbers(n) {
+    return n*n*n
+}
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    let combinedString = s1 + s2
+    let uniqueString = new Set(combinedString)
+    let sortedString = [...uniqueString].sort()
+    let output = sortedString.join('')
+    return output
+}
+
+// Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+// Square all numbers k (0 <= k <= n) between 0 and n.
+
+// Count the numbers of digits d used in the writing of all the k**2.
+
+// Implement the function taking n and d as parameters and returning this count.
+
+// Examples:
+// n = 10, d = 1 
+// the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+// We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+// The function, when given n = 25 and d = 1 as argument, should return 11 since
+// the k*k that contain the digit 1 are:
+// 1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+// So there are 11 digits 1 for the squares of numbers between 0 and 25.
+// Note that 121 has twice the digit 1.
+
+function nbDig(n, d) {
+    const squareList = []
+    const count = 0
+    for (let k = 0; k <= n; k ++) {
+        squareList.push(k*k)
+    }
+    for (let i = 0; i < squareList.length; i ++) {
+        const squareStr = squareList[i].toString()
+        for (let j = 0; j < squareStr.length; j ++) {
+            if (parseInt(squareStr[j]) === d) {
+                count ++
+            }
+        }
+    }
+    return count
+}
+
