c1968cde68c3da41c690217024ad8a80508fa81c
nhientattran <nhientattran@gmail.com>
2023-09-23 11:18:37 -0500

    09/23/2023

---
+++ 09_23.js
@@ -0,0 +1,27 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+function between(a, b) {
+    let out = []
+    for (let i = a; i <= b; i ++) {
+        out.push(i)
+    }
+    return out
+}
+
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Example:
+// move(3, 6) should equal 15
+
+function move(position, roll) {
+    return position + roll * 2
+}
